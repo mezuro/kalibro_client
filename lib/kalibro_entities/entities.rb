70a19202c13675e72b25e430559df7537f52a316
Rafael Reggiani Manzo + Guilherme Rojas V. de Lima <rr.manzo@gmail.com>
2013-07-31 14:52:36 -0300

    Stop to include entities by default

--- lib/kalibro_entities/entities.rb
+++ lib/kalibro_entities/entities.rb
@@ -15,5 +15,3 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'kalibro_entities/entities/project'
-
-include KalibroEntities::Entities
--- lib/kalibro_entities/errors.rb
+++ lib/kalibro_entities/errors.rb
@@ -16,5 +16,3 @@
 
 require 'kalibro_entities/errors/standard'
 require 'kalibro_entities/errors/record_not_found'
-
-include KalibroEntities::Errors
