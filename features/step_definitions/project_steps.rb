38ca8b6a4b194059074bfe60d4a7c6e829f2d756
Guilherme Rojas V. de Lima + Rafael Reggiani Manzo <guilhermehrojas@gmail.com>
2013-08-14 15:49:02 -0300

    Acceptance test for the find actionx

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -6,6 +6,10 @@ Given(/^the project name is "(.*?)"$/) do |name|
   @project.name = name
 end
 
+Given(/^I have a project with name "(.*?)"$/) do |name|
+  @project = KalibroEntities::Entities::Project.create({name: name})
+end
+
 When(/^I save the project$/) do
   @project.save
 end
@@ -14,6 +18,15 @@ When(/^I create the project with name "(.*?)"$/) do |name|
   @project = KalibroEntities::Entities::Project.create({name: name})
 end
 
+When(/^I search a project with the same id of the given project$/) do
+  @found_project = KalibroEntities::Entities::Project.find(@project.id)
+end
+
 Then(/^the project should exist$/) do
   KalibroEntities::Entities::Project.exists?(@project.id)
 end
+
+Then(/^it should return the same project as the given one$/) do
+  @found_project == @project
+end
+
