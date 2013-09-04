d8459adb47266dc309a71bdfc33e512c22a987f2
Rafael Reggiani Manzo + Guilher Rojas V. de Lima <rr.manzo@gmail.com>
2013-09-04 10:06:48 -0300

    Fixed kalibro hook
    It was printing some errors while running the tests

--- lib/kalibro_entities/kalibro_cucumber_helpers/scripts/delete_all_kalibro_entries.sh
+++ lib/kalibro_entities/kalibro_cucumber_helpers/scripts/delete_all_kalibro_entries.sh
@@ -20,4 +20,4 @@ PASSWORD=$1
 DATABASE=$2
 QUERYFILE=$3
 
-sudo su postgres -c "export PGPASSWORD=$PASSWORD && psql -q -d $DATABASE -f $QUERYFILE"
+sudo su - postgres -c "export PGPASSWORD=$PASSWORD && psql -q -d $DATABASE -f $QUERYFILE"
