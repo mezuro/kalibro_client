72dd0544f4bda9f5b167b0dbfc933952def7215b
Rafael Reggiani Manzo + Guilherme Rojas V. de Lima <rr.manzo@gmail.com>
2013-09-17 18:38:17 -0300

    Added the StackTraceElement entity

---
+++ spec/entities/stack_trace_element_spec.rb
@@ -0,0 +1,26 @@
+# This file is part of KalibroEntities
+# Copyright (C) 2013  it's respectives authors (please see the AUTHORS file)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'spec_helper'
+
+describe KalibroEntities::Entities::StackTraceElement do
+  describe 'line_number=' do
+    it 'should set the line_number' do
+      subject.line_number = 42
+      subject.line_number.should eq(42)
+    end
+  end
+end
