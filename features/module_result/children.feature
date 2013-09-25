42ba9c391cc48928380c373aaf787dba2f264a44
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2013-09-25 15:51:54 -0300

    Acceptance test for ModuleResult's children method

--- features/module_result/children.feature
+++ features/module_result/children.feature
@@ -3,9 +3,16 @@ Feature: Children
   As an developer
   I want to find children module results
 
-  @wip
   @kalibro_restart
   Scenario: find a valid module result
-    Given I have a module result
-    When I search for the children of a module result
+    Given I have a project with name "Kalibro"
+    And I have a configuration with name "Java"
+    And I have a reading group with name "Group"
+    And I have a metric configuration within the given configuration
+    And the given project has the following Repositories:
+      |   name    | type |              address                  |
+      |  Kalibro  |  GIT | https://github.com/mezuro/kalibro.git |
+    And I call the process method for the given repository
+    And I wait up for a ready processing
+    When I search for the children of the processing root module result
     Then it should return a list of the children module results
--- features/step_definitions/module_result_steps.rb
+++ features/step_definitions/module_result_steps.rb
@@ -8,15 +8,24 @@ When(/^I search a module result with the same id of the given module result$/) d
   @found_module_result = KalibroEntities::Entities::ModuleResult.find(@module_result.id)
 end
 
+When(/^I search an inexistent module result$/) do
+  @is_error = false
+  begin
+    KalibroEntities::Entities::ModuleResult.find(-1)
+  rescue KalibroEntities::Errors::RecordNotFound
+    @is_error = true
+  end
+end
+
 Then(/^it should return the same module result as the given one$/) do
   @found_module_result == @module_result
 end
 
-When(/^I search an inexistent module result$/) do
-  @is_error = false
-  begin 
-  	KalibroEntities::Entities::ModuleResult.find(-1)
-  rescue KalibroEntities::Errors::RecordNotFound
-  	@is_error = true 
-  end
+When(/^I search for the children of the processing root module result$/) do
+  @children = KalibroEntities::Entities::ModuleResult.
+    find(KalibroEntities::Entities::Processing.processing_of(@repository.id).results_root_id)
+end
+
+Then(/^it should return a list of the children module results$/) do
+  @children.should be_a(KalibroEntities::Entities::ModuleResult)
 end
