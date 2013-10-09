1fd01db7be5897e7055bc8b6eb449f9d0f35c4e4
Guilherme Rojas V. de Lima + Fellipe Souto <guilhermehrojas@gmail.com>
2013-10-09 18:06:48 -0300

    Refactored Metric Configuration

--- spec/entities/metric_configuration_spec.rb
+++ spec/entities/metric_configuration_spec.rb
@@ -18,9 +18,8 @@ require 'spec_helper'
 
 describe KalibroEntities::Entities::MetricConfiguration do
   describe 'id=' do
-    it 'should set the id' do
-      subject.id = 42
-
+    it 'should set the id as an Integer' do
+      subject.id = "42"
       subject.id.should eq(42)
     end
   end
@@ -28,43 +27,45 @@ describe KalibroEntities::Entities::MetricConfiguration do
   describe 'reading_group_id=' do
     it 'should set the reading group id' do
       subject.reading_group_id = "1"
-
       subject.reading_group_id.should eq(1)
     end
   end
 
   describe 'metric=' do
-    before do
-      @metric = FactoryGirl.build(:metric)
-      @metric_hash = @metric.to_hash
+    let(:metric) { FactoryGirl.build(:metric) }
+    
+    before :each do
+      KalibroEntities::Entities::Metric.
+        expects(:to_object).at_least_once.
+        with(metric.to_hash).
+        returns(metric)
     end
 
     it 'should convert the argument and set the metric' do
-      KalibroEntities::Entities::Metric.expects(:to_object).with(@metric_hash).returns(@metric)
-
-      subject.metric = @metric_hash
-      subject.metric.should eq(@metric)
+      subject.metric = metric.to_hash
+      subject.metric.should eq(metric)
     end
   end
 
   describe 'weight=' do
     it 'should set the weight' do
-      subject.weight = 10
-
+      subject.weight = "10"
       subject.weight.should eq(10)
     end
   end
 
   describe 'update_attributes' do
-    before do
-      @metric_configuration = FactoryGirl.build(:metric_configuration)
+    let(:metric_configuration) { FactoryGirl.build(:metric_configuration) }
+    
+    before :each do
+      KalibroEntities::Entities::MetricConfiguration.any_instance.
+        expects(:save).
+        returns(true)
     end
 
     it 'should set the attributes and save' do
-      KalibroEntities::Entities::MetricConfiguration.any_instance.expects(:save).returns(true)
-
-      subject.update_attributes(@metric_configuration.to_hash)
-      subject.id.should eq(@metric_configuration.id)
+      subject.update_attributes(metric_configuration.to_hash)
+      subject.code.should eq(metric_configuration.code)
     end
   end
 
@@ -77,21 +78,19 @@ describe KalibroEntities::Entities::MetricConfiguration do
   end
 
   describe 'find' do
-    before do
-      @metric_configuration = FactoryGirl.build(:metric_configuration)
-      @metric_configuration_hash = @metric_configuration.to_hash
-    end
+    let(:metric_configuration) { FactoryGirl.build(:metric_configuration) }
 
     context 'with and existant MetricConfiguration' do
       before :each do
         KalibroEntities::Entities::MetricConfiguration.
           expects(:request).
-          with(:get_metric_configuration, {:metric_configuration_id => @metric_configuration.id}).
-          returns({metric_configuration: @metric_configuration_hash})
+          with(:get_metric_configuration, {:metric_configuration_id => metric_configuration.id}).
+          returns({metric_configuration: metric_configuration.to_hash})
       end
 
       it 'should return the metric_configuration' do
-        KalibroEntities::Entities::MetricConfiguration.find(@metric_configuration.id).id.should eq(@metric_configuration.id)
+        KalibroEntities::Entities::MetricConfiguration.find(metric_configuration.id).
+          id.should eq(metric_configuration.id)
       end
     end
 
@@ -101,33 +100,33 @@ describe KalibroEntities::Entities::MetricConfiguration do
         any_error_message = ""
         KalibroEntities::Entities::MetricConfiguration.
           expects(:request).
-          with(:get_metric_configuration, {:metric_configuration_id => @metric_configuration.id}).
+          with(:get_metric_configuration, {:metric_configuration_id => metric_configuration.id}).
           raises(Savon::SOAPFault.new(any_error_message, any_code))
       end
 
       it 'should raise the RecordNotFound error' do
-        expect {KalibroEntities::Entities::MetricConfiguration.find(@metric_configuration.id)}.to raise_error(KalibroEntities::Errors::RecordNotFound)
+        expect {KalibroEntities::Entities::MetricConfiguration.find(metric_configuration.id)}.
+          to raise_error(KalibroEntities::Errors::RecordNotFound)
       end
     end
   end
 
   describe 'metric_configurations_of' do
-    before :each do
-      @metric_configuration = FactoryGirl.build(:metric_configuration)
-      @metric_configuration_hash = @metric_configuration.to_hash
-      @configuration_id = 42
+    let(:metric_configuration) { FactoryGirl.build(:metric_configuration) }
+    let(:configuration) { FactoryGirl.build(:configuration) }
 
+    before :each do
       KalibroEntities::Entities::MetricConfiguration.
         expects(:request).
-        with(:metric_configurations_of, {:configuration_id => @configuration_id}).
-        returns({metric_configuration: @metric_configuration_hash})
+        with(:metric_configurations_of, {:configuration_id => configuration.id}).
+        returns({metric_configuration: metric_configuration.to_hash})
     end
 
     it 'should return a array with a metric configuration' do
-      metric_configurations = KalibroEntities::Entities::MetricConfiguration.metric_configurations_of(@configuration_id)
+      metric_configurations = KalibroEntities::Entities::MetricConfiguration.metric_configurations_of(configuration.id)
 
       metric_configurations.should be_an(Array)
-      metric_configurations.first.id.should eq(@metric_configuration.id)
+      metric_configurations.first.id.should eq(metric_configuration.id)
     end
   end
 
@@ -137,7 +136,9 @@ describe KalibroEntities::Entities::MetricConfiguration do
 
     before :each do
       KalibroEntities::Entities::MetricConfiguration.
-        expects(:request).with(:save_metric_configuration, {:metric_configuration => subject.to_hash, :configuration_id => subject.configuration_id}).returns({:metric_configuration_id => 1})
+        expects(:request).
+        with(:save_metric_configuration, {:metric_configuration => subject.to_hash, :configuration_id => subject.configuration_id}).
+        returns({:metric_configuration_id => 1})
     end
 
     it 'should make a request to save model with id and return true without errors' do
