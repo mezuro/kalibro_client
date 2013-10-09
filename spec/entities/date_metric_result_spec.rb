54860b812bf7c9d049333330957edede01de0797
Guilherme Rojas V. de Lima <guilhermehrojas@gmail.com>
2013-10-09 10:59:32 -0300

    Date Metric Result refactored

--- spec/entities/date_metric_result_spec.rb
+++ spec/entities/date_metric_result_spec.rb
@@ -37,22 +37,23 @@ describe KalibroEntities::Entities::DateMetricResult do
   end
 
   describe 'metric_result=' do
-    before :each do
-      @metric_result = FactoryGirl.build(:metric_result)
-      @metric_result_hash = @metric_result.to_hash
+    let(:metric_result) { FactoryGirl.build(:metric_result) }
 
-      KalibroEntities::Entities::MetricResult.expects(:to_object).with(@metric_result_hash).returns(@metric_result)
+    before :each do
+      KalibroEntities::Entities::MetricResult.
+        expects(:to_object).
+        with(metric_result.to_hash).
+        returns(metric_result)
     end
 
     it 'should set the metric_result with the given one' do
-      subject.metric_result = @metric_result_hash
-
-      subject.metric_result.should eq(@metric_result)
+      subject.metric_result = metric_result.to_hash
+      subject.metric_result.should eq(metric_result)
     end
   end
 
   describe 'result' do
-    subject {FactoryGirl.build(:date_metric_result)}
+    subject { FactoryGirl.build(:date_metric_result) }
 
     it 'should return the metric result value' do
       subject.result.should eq(10)
