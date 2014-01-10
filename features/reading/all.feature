74fbc4596a8cd1d41d842e8bea98abb6d404c6ca
Fellipe Souto Sampaio <fllsouto@gmail.com>
2014-01-10 17:52:39 -0200

    Acceptance test for all and exist for reading feature
    Signed-off by: Diego Araujo <diegoamc90@gmail.com>

---
+++ features/reading/all.feature
@@ -0,0 +1,13 @@
+Feature: Reading listing
+  In order to be able to visualize readings
+  As a developer
+  I want to see all the readings on the service
+
+  @kalibro_restart
+  Scenario: With existing reading group reading
+    Given I have a reading group with name "Kalibro"
+    And the given reading group has the following readings:
+      |   label   | grade |     color    |
+      | "Awesome" |  10   |     3333FF   |
+    When I ask for all the readings
+    Then the response should contain the given reading
--- features/step_definitions/reading_steps.rb
+++ features/step_definitions/reading_steps.rb
@@ -1,5 +1,17 @@
+Given(/^the given reading group has the following readings:$/) do |table|
+  hash = table.hashes.first
+  hash[:group_id] = @reading_group.id
+  hash[:id] = nil
+
+  @reading = FactoryGirl.create(:reading, hash)
+end
+
 Given(/^I have a reading within the given reading group$/) do
   @reading = FactoryGirl.create(:reading, {id: nil, group_id: @reading_group.id})
+end
+
+When(/^I ask for all the readings$/) do
+  @all_readings = KalibroGem::Entities::Reading.all
 end
 
 When(/^I ask for a reading with the same id of the given reading$/) do
@@ -10,6 +22,10 @@ When(/^I ask for the readings of the given reading group$/) do
   @found_readings = KalibroGem::Entities::Reading.readings_of @reading_group.id
 end
 
+When(/^I ask to check if the given reading exists$/) do
+  @response = KalibroGem::Entities::Reading.exists?(@reading.id)
+end
+
 Then(/^I should get the given reading$/) do
   @found_reading == @reading
 end
@@ -18,3 +34,6 @@ Then(/^I should get a list with the given reading$/) do
   @found_readings.first == @reading
 end
 
+Then(/^the response should contain the given reading$/) do
+  @all_readings.first == @reading
+end
