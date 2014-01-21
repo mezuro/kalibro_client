5c9314b9c6b48892923302669916954ece83c5d3
Rafael Reggiain Manzo <rr.manzo@gmail.com>
2014-01-20 22:24:05 -0200

    Grant all permissions on test folder avoiding errors on some machines

--- lib/kalibro_gem/kalibro_cucumber_helpers/scripts/put_kalibro_on_test_mode.sh
+++ lib/kalibro_gem/kalibro_cucumber_helpers/scripts/put_kalibro_on_test_mode.sh
@@ -28,8 +28,11 @@ echo "-->  Creating tests directory"
 sudo mkdir $KALIBRO_HOME/tests
 echo "-->  Copying test settings"
 sudo cp $KALIBRO_HOME/kalibro_test.settings $KALIBRO_HOME/tests/kalibro.settings
+echo "-->  Copying log folder"
+sudo cp $KALIBRO_HOME/kalibro_test.settings $KALIBRO_HOME/tests/kalibro.settings
 echo "-->  Changing owner of tests directory to tomcat6"
 sudo chown -R $TOMCAT_USER:$TOMCAT_GROUP $KALIBRO_HOME/tests
+sudo chmod 777 -R $KALIBRO_HOME/tests
 
 # you must restart tomcat6
 $TOMCAT_RESTART_COMMAND
