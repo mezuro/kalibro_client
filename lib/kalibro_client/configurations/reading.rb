c9ab37ce0df3d18b9789832a4feccfb1a6678c81
Diego Araújo <diegoamc90@gmail.com>
2014-11-12 16:25:29 -0200

    Included some associations

--- lib/kalibro_client/configurations/reading.rb
+++ lib/kalibro_client/configurations/reading.rb
@@ -1,6 +1,8 @@
 module KalibroClient
   module Configurations
     class Reading < Base
+      belongs_to :reading_group, class_name: KalibroClient::Configurations::ReadingGroup
+      has_many :kalibro_ranges, class_name: KalibroClient::Configurations::KalibroRange
     end
   end
 end
