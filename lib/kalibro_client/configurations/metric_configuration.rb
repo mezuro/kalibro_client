be0c210825c7a36f3fc8c17c4117f611c9a16f91
Heitor Reis <marcheing@gmail.com>
2014-11-14 12:48:56 -0200

    Added association in metric configuration, fixed name of KalibroRange module

--- lib/kalibro_client/configurations/metric_configuration.rb
+++ lib/kalibro_client/configurations/metric_configuration.rb
@@ -2,7 +2,8 @@ module KalibroClient
   module Configurations
     class MetricConfiguration < Base
       belongs_to :metric_snapshot, class_name: KalibroClient::Configurations::MetricSnapshot
-      has_many :kalibro_ranges, class_name: KalibroClient::Configurations::KalibroRangesS
+      belongs_to :kalibro_configuration, class_name: KalibroClient::Configurations::KalibroConfiguration
+      has_many :kalibro_ranges, class_name: KalibroClient::Configurations::KalibroRange
     end
   end
 end
