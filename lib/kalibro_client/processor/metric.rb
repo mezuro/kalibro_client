2fdafc0578f7faf325b0b4dc89f07ddc1d84c541
Heitor Reis <marcheing@gmail.com>
2014-12-11 15:38:58 -0200

    Changed metric initialization to create a proper ActiveResource::Base object
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- lib/kalibro_client/processor/metric.rb
+++ lib/kalibro_client/processor/metric.rb
@@ -1,14 +1,8 @@
 module KalibroClient
   module Processor
     class Metric < Base
-      attr_accessor :compound, :name, :code, :scope, :description
-
       def initialize(compound, name, code, scope)
-        @compound = compound
-        @name = name
-        @code = code
-        @scope = scope
-        @description = ""
+        super(compound: compound, name: name, code: code, scope: scope)
       end
     end
   end
