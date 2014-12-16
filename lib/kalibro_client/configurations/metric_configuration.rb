cb9fc87a0de615c10d6b1546f4b02914482086c0
Diego Araújo <diegoamc90@gmail.com>
2014-12-16 15:41:21 -0200

    Default value for weight on metric configuration's constructor.

--- lib/kalibro_client/configurations/metric_configuration.rb
+++ lib/kalibro_client/configurations/metric_configuration.rb
@@ -5,7 +5,7 @@ module KalibroClient
       belongs_to :kalibro_configuration, class_name: KalibroClient::Configurations::KalibroConfiguration
       has_many :kalibro_ranges, class_name: KalibroClient::Configurations::KalibroRange
 
-      def initialize(weight)
+      def initialize(weight=0.0)
         super(weight: weight)
       end
     end
