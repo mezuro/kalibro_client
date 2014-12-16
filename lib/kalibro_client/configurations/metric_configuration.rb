703917091a168b7d4558e53ccde0fdf42e643083
Diego Araújo <diegoamc90@gmail.com>
2014-12-16 15:54:15 -0200

    Included reading group id on Metric Configuration.

--- lib/kalibro_client/configurations/metric_configuration.rb
+++ lib/kalibro_client/configurations/metric_configuration.rb
@@ -5,8 +5,8 @@ module KalibroClient
       belongs_to :kalibro_configuration, class_name: KalibroClient::Configurations::KalibroConfiguration
       has_many :kalibro_ranges, class_name: KalibroClient::Configurations::KalibroRange
 
-      def initialize(weight=0.0, aggregation_form=:AVERAGE)
-        super(weight: weight, aggregation_form: aggregation_form)
+      def initialize(weight=0.0, aggregation_form=:AVERAGE, reading_group_id=nil)
+        super(weight: weight, aggregation_form: aggregation_form, reading_group_id: reading_group_id)
       end
     end
   end
