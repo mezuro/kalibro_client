343cbef2a321c9a72d3f1048760372d92106a2c8
Daniel Alves <danpaulalves@gmail.com>
2014-12-17 15:35:49 -0200

    Changed metric configuration constructor according to validation.
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- lib/kalibro_client/configurations/metric_configuration.rb
+++ lib/kalibro_client/configurations/metric_configuration.rb
@@ -5,7 +5,7 @@ module KalibroClient
       belongs_to :kalibro_configuration, class_name: KalibroClient::Configurations::KalibroConfiguration
       has_many :kalibro_ranges, class_name: KalibroClient::Configurations::KalibroRange
 
-      def initialize(weight=0.0, aggregation_form=:AVERAGE, reading_group_id=nil)
+      def initialize(weight=1.0, aggregation_form=:AVERAGE, reading_group_id=nil)
         super(weight: weight, aggregation_form: aggregation_form, reading_group_id: reading_group_id)
       end
     end
