2bd9e22c1bd5dd8227ec1475d6f1b522e716b009
Diego Araújo <diegoamc90@gmail.com>
2015-01-12 16:31:54 -0200

    Configured cucumber environment.
    Signed off by: Daniel Alves <danpaulalves@gmail.com>
    Signed off by: Rafael Manzo <rr.manzo@gmail.com>

--- lib/kalibro_client/kalibro_cucumber_helpers/configuration.rb
+++ lib/kalibro_client/kalibro_cucumber_helpers/configuration.rb
@@ -17,21 +17,9 @@
 module KalibroClient
   module KalibroCucumberHelpers
     class Configuration
-      attr_accessor :database, :user, :password,
-                    :query_file_path, :psql_file_path, :kalibro_home,
-                    :tomcat_user, :tomcat_group, :tomcat_restart_command,
-                    :kalibro_processor_address, :kalibro_configurations_address
+      attr_accessor :kalibro_processor_address, :kalibro_configurations_address
 
       def initialize(attributes={})
-        self.database                  = "kalibro_test"
-        self.user                      = "kalibro"
-        self.password                  = "kalibro"
-        self.psql_file_path            = "/tmp/PostgreSQL.sql"
-        self.query_file_path           = "/tmp/query"
-        self.kalibro_home              = "/usr/share/tomcat6/.kalibro"
-        self.tomcat_user               = "tomcat6"
-        self.tomcat_group              = "tomcat6"
-        self.tomcat_restart_command    = "sudo\\ service\\ tomcat6\\ restart"
         self.kalibro_processor_address = "http://localhost:8082"
         self.kalibro_configurations_address = "http://localhost:8083"
 
--- lib/kalibro_client/kalibro_cucumber_helpers/hooks.rb
+++ lib/kalibro_client/kalibro_cucumber_helpers/hooks.rb
@@ -16,49 +16,10 @@
 
 require 'kalibro_client/kalibro_cucumber_helpers'
 
-Before do
-  if !$dunit
-    command = "#{__dir__}/scripts/put_kalibro_on_test_mode.sh \\
-                #{KalibroClient::KalibroCucumberHelpers.configuration.kalibro_home} \\
-                #{KalibroClient::KalibroCucumberHelpers.configuration.tomcat_restart_command} \\
-                #{KalibroClient::KalibroCucumberHelpers.configuration.tomcat_user} \\
-                #{KalibroClient::KalibroCucumberHelpers.configuration.tomcat_group}"
-    system command
-
-    command = "#{__dir__}/scripts/prepare_kalibro_query_file.sh \\
-                #{KalibroClient::KalibroCucumberHelpers.configuration.psql_file_path} \\
-                #{KalibroClient::KalibroCucumberHelpers.configuration.query_file_path}"
-    system command
-
-    command = "#{__dir__}/scripts/delete_all_kalibro_entries.sh \\
-               #{KalibroClient::KalibroCucumberHelpers.configuration.password} \\
-               #{KalibroClient::KalibroCucumberHelpers.configuration.database} \\
-               #{KalibroClient::KalibroCucumberHelpers.configuration.query_file_path}"
-    system command
-
-    $dunit = true
-  end
-end
-
-After ('@kalibro_restart') do
-  command = "#{__dir__}/scripts/delete_all_kalibro_entries.sh \\
-               #{KalibroClient::KalibroCucumberHelpers.configuration.password} \\
-               #{KalibroClient::KalibroCucumberHelpers.configuration.database} \\
-               #{KalibroClient::KalibroCucumberHelpers.configuration.query_file_path}"
-  system command
-end
-
 After('@kalibro_processor_restart') do
   KalibroClient::KalibroCucumberHelpers.clean_processor
 end
 
-After('@kalibro_configurations_restart') do
+After('@kalibro_configuration_restart') do
   KalibroClient::KalibroCucumberHelpers.clean_configurations
-end
-
-at_exit do
-  command = "#{__dir__}/scripts/return_kalibro_from_test_mode.sh \\
-                #{KalibroClient::KalibroCucumberHelpers.configuration.kalibro_home} \\
-                #{KalibroClient::KalibroCucumberHelpers.configuration.tomcat_restart_command}"
-  system command
 end
