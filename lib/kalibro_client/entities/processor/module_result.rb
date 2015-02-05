a05cfd6ff60275ce32800349a7a1caacc112866f
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-02-05 11:31:01 -0200

    ModuleResult processing method

--- lib/kalibro_client/entities/processor/module_result.rb
+++ lib/kalibro_client/entities/processor/module_result.rb
@@ -51,6 +51,10 @@ module KalibroClient
           @processing_id = value.to_i
         end
 
+        def processing
+          KalibroClient::Entities::Processor::Processing.find(self.processing_id)
+        end
+
         def grade=(value)
           @grade = value.to_f
         end
