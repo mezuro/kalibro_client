0dbd15aac16f84b3146852761b972c4dd82a73bb
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-10-14 15:02:32 -0300

    Add features for MetricConfiguration retrieval
    There will be methods for listing specifically KalibroCOnfiguration's
    tree or hotspot MetricConfigurations.

--- features/step_definitions/metric_configuration_steps.rb
+++ features/step_definitions/metric_configuration_steps.rb
@@ -18,6 +18,18 @@ Given(/^I have a loc configuration within the given kalibro configuration$/) do
                                               kalibro_configuration_id: @kalibro_configuration.id})
 end
 
+Given(/^I have a hotspot metric configuration within the given kalibro configuration$/) do
+  @hotspot_metric_configuration = FactoryGirl.create(:metric_configuration,
+                                                     {metric: FactoryGirl.build(:hotspot_metric),
+                                                      reading_group_id: @reading_group.id,
+                                                      kalibro_configuration_id: @kalibro_configuration.id})
+end
+
+Given(/^I have a tree metric configuration within the given kalibro configuration$/) do
+  step "I have a loc configuration within the given kalibro configuration"
+  @tree_metric_configuration = @metric_configuration
+end
+
 When(/^I search a metric configuration with the same id of the given metric configuration$/) do
   @found_metric_configuration = KalibroClient::Entities::Configurations::MetricConfiguration.find(@metric_configuration.id)
 end
@@ -49,6 +61,14 @@ When(/^I have a flay configuration within the given kalibro configuration$/) do
                                               kalibro_configuration_id: @kalibro_configuration.id})
 end
 
+When(/^I request for hotspot_metric_configurations of the given kalibro configuration$/) do
+  @hotspot_metric_configurations = @kalibro_configuration.hotspot_metric_configurations
+end
+
+When(/^I request for tree_metric_configurations of the given kalibro configuration$/) do
+  @tree_metric_configurations = @kalibro_configuration.tree_metric_configurations
+end
+
 Then(/^it should return the same metric configuration as the given one$/) do
   expect(@found_metric_configuration).to eq(@metric_configuration)
 end
@@ -72,4 +92,12 @@ end
 
 Then(/^its metric should be Hotspot one$/) do
   expect(@found_metric_configuration.metric).to be_a(KalibroClient::Entities::Miscellaneous::HotspotMetric)
+end
+
+Then(/^I should get a list with the given HotspotMetricConfiguration$/) do
+  expect(@hotspot_metric_configurations).to include(@hotspot_metric_configuration)
+end
+
+Then(/^I should get a list with the given TreeMetricConfiguration$/) do
+  expect(@tree_metric_configurations).to include(@tree_metric_configuration)
 end
