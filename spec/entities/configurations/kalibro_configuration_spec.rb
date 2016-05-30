aee95552484e6976958753b1435eb9a8fbfe0b7b
Daniel Miranda <danielkza2@gmail.com>
2016-05-30 17:47:14 -0300

    Add language attribute to KalibroConfiguration

--- spec/entities/configurations/kalibro_configuration_spec.rb
+++ spec/entities/configurations/kalibro_configuration_spec.rb
@@ -17,6 +17,14 @@
 require 'spec_helper'
 
 describe KalibroClient::Entities::Configurations::KalibroConfiguration do
+  describe 'accessors' do
+    it 'should have accessors for the needed attributes' do
+      [:name, :description, :language].each do |attr|
+        expect(subject).to have_attr_accessor(attr)
+      end
+    end
+  end
+
   describe 'id=' do
     it 'should set the value of the attribute id as an Integer' do
       subject.id = "42"
