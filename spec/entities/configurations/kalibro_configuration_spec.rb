55b595d203d71d93ecd6eb314eeb3ea70a373c73
Eduardo Silva Araújo <duduktamg@hotmail.com>
2016-06-04 21:57:31 -0300

    Use more specific messages on accessor tests
    Since we use tests as documentation, it is nice to have more informative
    messages.

--- spec/entities/configurations/kalibro_configuration_spec.rb
+++ spec/entities/configurations/kalibro_configuration_spec.rb
@@ -18,8 +18,8 @@ require 'spec_helper'
 
 describe KalibroClient::Entities::Configurations::KalibroConfiguration do
   describe 'accessors' do
-    it 'should have accessors for the needed attributes' do
-      [:name, :description, :language].each do |attr|
+    [:name, :description, :language].each do |attr|
+      it 'should have accessors for the #{attr} attribute' do
         expect(subject).to have_attr_accessor(attr)
       end
     end
